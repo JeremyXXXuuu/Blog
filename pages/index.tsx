abe5c3d71fee65ebbd1cae4d8b5127bbcf08c131
jeremyxu <jeremyxu1234@gmail.com>
2023-03-01 13:12:54 +0100

    init repo

---
+++ pages/index.tsx
@@ -0,0 +1,64 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import { posts } from '../lib/notion'
+
+export async function getServerSideProps() {
+    // Get the posts
+    let { results } = await posts();
+    // Return the result
+    return {
+        props: {
+            posts: results,
+            
+        }
+    }
+}
+
+interface Props {
+    posts: [any]
+}
+
+const Home: NextPage<Props> = (props) => {
+    console.log(props.posts)
+    return (
+        <div className={styles.container}>
+        <Head>
+            <title>Latest posts</title>
+        </Head>
+
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+            Jeremy's Blog
+            </h1>
+            { 
+                props.posts.filter((post) => post.cover!=null && post.properties.Published.checkbox === true).map((result,index) => {
+                    console.log(JSON.stringify(result));
+                return (
+                    <div className={styles.cardHolder} key={index}>
+                    <Link href={`/posts/${result.id}`}>
+                        <Image src={result.cover.external.url}  width={300} height={200} />
+                    </Link>
+                    <div className={styles.cardContent}>
+                        <Link href={`/posts/${result.id}`}>
+                        <a className={styles.cardTitle}>{
+                        result.properties.Name.rich_text[0].plain_text
+                        }</a>
+                        </Link>
+                    </div>
+                    </div>
+                    )
+                })
+            }
+        </main>
+
+        <footer className={styles.footer}>
+            <p>Blog application</p>
+        </footer>
+        </div>
+    )
+}
+
+export default Home
