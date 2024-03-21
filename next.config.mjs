9060be5271a5b9827b8e86ee66071894d9efb87e
jeremyxxxuuu <jeremyxu1234@gmail.com>
2024-03-21 16:59:14 +0100

    add mdx & contentlayer

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+import { withContentlayer } from "next-contentlayer";
+
 const nextConfig = {
   images: {
     domains: [
       "images.unsplash.com",
       "www.notion.so",
       "s3.us-west-2.amazonaws.com",
-      "prod-files-secure.s3.us-west-2.amazonaws.com"
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
   },
 };
 
-module.exports = nextConfig;
+export default withContentlayer(nextConfig);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -18,6 +18,12 @@ const config = {
       },
     },
     extend: {
+      textColor: {
+        primary: "var(--gray-12)",
+        secondary: "var(--gray-11)",
+        tertiary: "var(--gray-9)",
+        brand: "var(--brand)",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -74,7 +80,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+} satisfies Config;
 
-export default config
+export default config;
